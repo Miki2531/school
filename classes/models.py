dcb3309fdb380f19fcda6589332d1d713b6e330c
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-13 23:11:34 +0300

    create tuble for classroom choice

--- classes/models.py
+++ classes/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Class_room(models.Model):
-    roomName = models.CharField(max_length=50, null=False, blank=False)
+    GreadClass_chocice = (
+        ('Gread 9','G9'),
+        ('Gread 10', 'G10'),
+        ('Gread 11', 'G11'),
+        ('Gread 12', 'G12')
+    )
+    roomName = models.CharField(max_length=50, null=False, blank=False,  choices=GreadClass_chocice)
     def __str__(self):
         return self.roomName
 class Subject(models.Model):
