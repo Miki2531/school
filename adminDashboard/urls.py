ba194e768c3fdce4412c4497ee7970cf37cb3eee
mgeza <mgeza@ZOO>
2023-08-14 21:01:41 +0300

    url path to User register views

--- adminDashboard/urls.py
+++ adminDashboard/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 app_name = 'admin_dash'
 urlpatterns = [
     path('', views.admin_dash, name='admin_dash'),
+    path('register/', views.register_user, name='register'),
 ]
