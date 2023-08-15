80f10077fe33d0d80ef414af3ee4d8f2f649396d
mgeza <mgeza@ZOO>
2023-08-15 23:20:52 +0300

    url patter to student creation view

--- adminDashboard/urls.py
+++ adminDashboard/urls.py
@@ -4,4 +4,5 @@ app_name = 'admin_dash'
 urlpatterns = [
     path('', views.admin_dash, name='admin_dash'),
     path('register/', views.register_user, name='register'),
+    path('create_student/', views.create_student, name='create_student'),
 ]
--- adminDashboard/views.py
+++ adminDashboard/views.py
@@ -7,7 +7,7 @@ from accounts.decorators import admin_required
 from students.models import Student
 from teachers.models import Teacher
 from classes.models import Class_room, Subject
-from .forms import CustomUserRegistrationForm
+from .forms import CustomUserRegistrationForm, StudentForm, FamilyForm
 
 @admin_required
 def admin_dash(request):
@@ -36,3 +36,23 @@ def register_user(request):
 
     context = {'form': form}
     return render(request, 'adminDashboard/register.html', context)
+
+@admin_required
+def create_student(request):
+    if request.method == 'POST':
+        student_form = StudentForm(request.POST)
+        family_form = FamilyForm(request.POST)
+
+        if student_form.is_valid() and family_form.is_valid():
+            print(StudentForm(request.POST).data)
+            family = family_form.save()
+            student = student_form.save(commit=False)
+
+            student.family_id = family
+            student.save()
+            return redirect('admin_dash:admin_dash')
+    else:
+        student_form = StudentForm()
+        family_form = FamilyForm()
+
+    return render(request, 'adminDashboard/create_student.html', {'student_form': student_form, 'family_form': family_form})
