9b593e5f12694db3699acb15bf4a749c2b1b77f0
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-16 22:07:22 +0300

    new change come

--- adminDashboard/forms.py
+++ adminDashboard/forms.py
@@ -1,47 +1,6 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-<<<<<<< HEAD
-from accounts.models import CustomUser
-from students.models import Family, Family_type, Student  # Import your CustomUser model if it exists
-
-class CustomUserRegistrationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser  # Use your CustomUser model here
-        fields = ['username', 'email', 'user_role']  # Add other fields as needed
-
-
-class FamilyTypeSelector(ModelForm):
-    class Meta:
-        model = Family_type
-        fields = [
-            'typeName'
-        ]
-        widgets = {
-            'typeName': forms.Select()
-        }
-
-class FamilyForm(ModelForm):
-    class Meta:
-        model = Family
-        fields = [
-            'firstName', 'middelName', 'lastName', 'email', 'phone', 'sex', 'family_type_id'
-        ]
-
-        widgets = {
-            'sex': forms.Select()
-        }
-class StudentForm(ModelForm):
-    class Meta:
-        model = Student
-        fields = [
-            'firstName', 'middelName', 'lastName', 'email', 'phone', 'sex', 'class_room_id', 'family_type_id'
-        ]
-
-        widgets = {
-            'sex': forms.Select()
-        }
-=======
 from accounts.models import CustomUser  # Import your CustomUser model if it exists
 from students.models import Student, Family
 
@@ -59,4 +18,4 @@ class FamilyForm(forms.ModelForm):
     class Meta:
         model = Family
         fields = '__all__'
->>>>>>> 1eeb01eeee01b72d8f3b5c559154d3ba8f51db2e
+
--- adminDashboard/views.py
+++ adminDashboard/views.py
@@ -5,13 +5,8 @@ from accounts.models import CustomUser
 from students.models import Student
 from teachers.models import Teacher
 from classes.models import Class_room, Subject
-<<<<<<< HEAD
-from .forms import CustomUserRegistrationForm
-from .forms import FamilyTypeSelector, FamilyForm, StudentForm
-=======
 from .forms import CustomUserRegistrationForm, StudentForm, FamilyForm
 from .managers import CustomUserManager
->>>>>>> 1eeb01eeee01b72d8f3b5c559154d3ba8f51db2e
 
 @admin_required
 def admin_dash(request):
@@ -41,48 +36,6 @@ def register_user(request):
     context = {'form': form}
     return render(request, 'adminDashboard/register.html', context)
 
-<<<<<<< HEAD
-
-
-
-def family_type_selector_view(request):
-    if request.method == 'POST':
-        form = FamilyTypeSelector(request.POST)
-        if form.is_valid():
-            selected_type = form.cleaned_data['typeName']
-            if selected_type == 'Parent':
-                return redirect('family_registration')
-            elif selected_type == 'Guardian':
-                return redirect('student_registration')
-            else:
-                return redirect('invalid_selection')
-    else:
-        form = FamilyTypeSelector()
-    
-    return render(request, 'family_type_selector.html', {'form': form})
-
-def family_registration_view(request):
-    if request.method == 'POST':
-        form = FamilyForm(request.POST)
-        if form.is_valid():
-            family = form.save()  
-            return redirect('family_registration_success')
-    else:
-        form = FamilyForm()
-    
-    return render(request, 'adminDashboard\family_registration.html', {'form': form})
-
-def student_registration_view(request):
-    if request.method == 'POST':
-        form = StudentForm(request.POST)
-        if form.is_valid():
-            student = form.save()  
-            return redirect('student_registration_success')
-    else:
-        form = StudentForm()
-    
-    return render(request, 'adminDashboard\student_registration.html', {'form': form})
-=======
 @admin_required
 def create_student(request):
     if request.method == 'POST':
@@ -112,4 +65,4 @@ def create_student(request):
         family_form = FamilyForm()
 
     return render(request, 'adminDashboard/create_student.html', {'student_form': student_form, 'family_form': family_form})
->>>>>>> 1eeb01eeee01b72d8f3b5c559154d3ba8f51db2e
+
