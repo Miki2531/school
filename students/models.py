9b7a9b39f03d0e2c3bbda6610e983eb707f959cf
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-16 22:14:07 +0300

    fix

--- students/models.py
+++ students/models.py
@@ -2,42 +2,12 @@ from django.db import models
 from classes.models import Class_room
 
 # Create your models here.
-<<<<<<< HEAD
 sex_choice = (
         ('Male', 'Male'),
         ('Female', 'Female')
     )
 
 class Family_type(models.Model):
-=======
-class Family(models.Model):
-    first_name_f     = models.CharField(max_length=50, null=False, blank=False)
-    last_name_f    = models.CharField(max_length=50, null=False, blank=False)
-    email_f         = models.EmailField(max_length=100, null=False, blank=False)
-    phone_f = models.CharField(max_length=15, blank=False, null=False)
-    gender_choices = [
-        ('M', 'Male'),
-        ('F', 'Female')
-    ]
-    gender_f = models.CharField(max_length=1, choices=gender_choices)
-    address_f = models.TextField(blank=True, null=True)
-    def __str__(self):
-        return self.first_name_f
-class Student(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
-    gender_choices = [
-        ('M', 'Male'),
-        ('F', 'Female')
-    ]
-    gender = models.CharField(max_length=1, choices=gender_choices)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    register_date = models.DateField(auto_now_add=True)
-    class_room_id = models.ForeignKey(Class_room, on_delete=models.SET_NULL, null=True)
->>>>>>> 1eeb01eeee01b72d8f3b5c559154d3ba8f51db2e
     Family_type_choice = (
         ('Mother', 'Mother'),
         ('Father', 'Father'),
@@ -47,11 +17,10 @@ class Student(models.Model):
         ('GrandPa', 'GrandPa')
     )
     typeName = models.CharField(max_length=50, null=False, blank=False, choices=Family_type_choice)
-    family_id = models.ForeignKey(Family, on_delete=models.SET_NULL, blank= True
-                                  , null=True)
+    # family_id = models.ForeignKey(Family, on_delete=models.SET_NULL, blank= True
+    #                               , null=True)
     def __str__(self):
-<<<<<<< HEAD
-        return self.typeName
+     return self.typeName
 class Family(models.Model):
     firstName     = models.CharField(max_length=50, null=False, blank=False)
     middelName    = models.CharField(max_length=50, null=False, blank=False)
@@ -74,9 +43,8 @@ class Student(models.Model):
     family_id = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.firstName
-=======
         return self.first_name
->>>>>>> 1eeb01eeee01b72d8f3b5c559154d3ba8f51db2e
+
     
 
 
