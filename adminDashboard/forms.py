c7faf9e04e914c17d4aec27d8e84f5ad46bed99e
mgeza <mgeza@ZOO>
2023-08-17 23:28:49 +0300

    from for schoolYearClass and SchoolYear add and database makemigration files are included in this commit

--- adminDashboard/forms.py
+++ adminDashboard/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from accounts.models import CustomUser  # Import your CustomUser model if it exists
 from students.models import Student, Family
+from .models import School_Year_Class, SchoolYear
 
 class CustomUserRegistrationForm(UserCreationForm):
     class Meta:
@@ -12,7 +13,14 @@ class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = '__all__'
-
+class SchoolYearClassForm(forms.ModelForm):
+    class Meta:
+        model = School_Year_Class
+        fields = '__all__'
+class SchoolYearForm(forms.ModelForm):
+    class Meta:
+        model = SchoolYear
+        fields = '__all__'
 class FamilyForm(forms.ModelForm):
     class Meta:
         model = Family
