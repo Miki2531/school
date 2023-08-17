c55034e3d995112f9ac5c239b923c51274c88ac5
mgeza <mgeza@ZOO>
2023-08-17 23:24:24 +0300

    School_year_class registered to admin site

--- adminDashboard/admin.py
+++ adminDashboard/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
-
+from django.contrib ifrom .models import School_Year_Class, SchoolYear
 # Register your models here.
+
+admin.site.register(School_Year_Class)
+admin.site.register(SchoolYear)
+
+
