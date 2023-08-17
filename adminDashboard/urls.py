58bfcc980a1bf35d5a6a858d9c31b6577f37cef5
mgeza <mgeza@ZOO>
2023-08-17 23:47:39 +0300

    url path added to school_year view

--- adminDashboard/urls.py
+++ adminDashboard/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.admin_dash, name='admin_dash'),
     path('register/', views.register_user, name='register'),
     path('create_student/', views.create_student, name='create_student'),
-    path('create_employee/', views.create_employee, name='create_employee')
+    path('create_employee/', views.create_employee, name='create_employee'),
+    path('create_school_year/', views.SchoolYear, name='school_year'),
 ]
