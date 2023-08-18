bb29eaed5b3ec628ad0e466301a6e776c9b8918c
Miki2531 <mikiyasshemelis46@gmail.com>
2023-08-18 15:00:43 +0300

    fix model repetition

--- employees/models.py
+++ employees/models.py
@@ -25,16 +25,3 @@ class Employees(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False, choices=Job_Type)
     def __str__(self):
         return self.title
-class Employees(models.Model):
-    firstName = models.CharField(max_length=50, null=False, blank=False)
-    middelName = models.CharField(max_length=50, null=False, blank=False)
-    lastName = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=100, null=False, blank=False)
-    phone = models.IntegerField(null=False, blank=False)
-    address = models.CharField(max_length=50, null=False, blank=False)
-    sex = models.CharField(max_length=10, null=False, blank=False)
-    registrationDate = models.DateField(auto_now_add= True)
-    Job_id = models.ForeignKey(Job, on_delete= models.SET_NULL, null=True)
-
-    def __str__(self):
-        return self.firstName
