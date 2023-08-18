4569f783b14cf68ec5dbec4bf3390c4a01d35983
mgeza <mgeza@ZOO>
2023-08-18 18:25:45 +0300

    typo fix from yearform to year_form

--- adminDashboard/models.py
+++ adminDashboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from classes.models import Class_student
 # Create your models here.
 
-class SchoolYear(models.Model):
+class School_Year(models.Model):
     year_name = models.CharField(max_length=9, unique=True, help_text="Format: YYYY-YYYY (e.g., 2023-2024)")
 
     def __str__(self):
@@ -11,7 +11,7 @@ class SchoolYear(models.Model):
     class Meta:
         verbose_name_plural = "School Years"
 class School_Year_Class(models.Model):
-    schoolYear = models.ForeignKey(SchoolYear, on_delete=models.SET_NULL, null=True, blank=False)
+    schoolYear = models.ForeignKey(School_Year, on_delete=models.SET_NULL, null=True, blank=False)
     student_in_class = models.ForeignKey(Class_student, on_delete=models.SET_NULL, null=True, blank=False)
     free = models.IntegerField()
 
