eb67fc3d4d0f0388d8874c6990904985905f85e0
mgeza <mgeza@ZOO>
2023-08-18 18:23:31 +0300

    attendace model removed from admin site

--- adminDashboard/admin.py
+++ adminDashboard/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import School_Year_Class, SchoolYear
+from .models import School_Year_Class, School_Year
 # Register your models here.
 
 admin.site.register(School_Year_Class)
-admin.site.register(SchoolYear)
+admin.site.register(School_Year)
 
 
