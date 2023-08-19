8a10d62a691ccfb3e0f9bed3e1113c865011361a
mgeza <mgeza@ZOO>
2023-08-19 11:43:17 +0300

    test.html to base.html

--- students/views.py
+++ students/views.py
@@ -1,10 +1,6 @@
-# from django.shortcuts import render
-# from accounts.decorators import student_required
-# # Create your views here.
-# @student_required
-# def student_dashboard(request):
-#     student = request.user  # Assuming the logged-in user is a student
-#     context = {
-#         'student': student,
-#     }
-#     return render(request, 'students/student_dashboard.html', context)
+from django.shortcuts import render
+from accounts.decorators import student_required
+# Create your views here.
+@student_required
+def student_dashboard(request):
+    return render(request, 'students/student_dashboard.html')
